6844da5991358ead4d35ea5f325ca629d5f00b8f
Diana <dm.gutierrez11@uniandes.edu.co>
2019-09-04 11:25:07 -0500

    Update check.js

--- check.js
+++ check.js
@@ -1,4 +1,17 @@
-if (!('indexedDB' in window)) {
+(function() {
+    'use strict';
+  
+    //check for support
+    if (!('indexedDB' in window)) {
+      console.log('This browser doesn\'t support IndexedDB');
+      return;
+    }
     console.log('This browser doesn\'t support IndexedDB');
-    return;
-  }
+    var dbPromise = idb.open('kobaDB', 1, function(upgradeDb) {
+      console.log('making a new object store');
+      if (!upgradeDb.objectStoreNames.contains('persona')) {
+        upgradeDb.createObjectStore('persona');
+      }
+    });
+  
+  })();
