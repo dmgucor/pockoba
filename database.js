125229af16c200a3c96d7c22c197455760a005b7
Diana <dm.gutierrez11@uniandes.edu.co>
2019-09-05 18:23:41 -0500

    final?

---
+++ database.js
@@ -0,0 +1,126 @@
+var previous = true;
+setInterval(function () {
+    var ifConnected = window.navigator.onLine;
+    if (ifConnected) {
+        console.log("online");
+        if (ifConnected !== previous) { connectionAlert(); }
+        previous = ifConnected;
+    } else {
+        console.log("offline");
+        previous = ifConnected;
+    }
+}, 3000);
+
+const dbName = "kobaDB";
+const transladosData = [
+    { producto: "Cerveza Andina", cantidad: "10", destinatario: "tienda", fecha: "02/02/19", motivo: "La fecha de vencimiento está muy próxima." }
+];
+
+var request = indexedDB.open(dbName, 2);
+var db;
+
+request.onerror = function (event) {
+    alert("Database error: " + event.target.errorCode);
+};
+
+request.onsuccess = function (evt) {
+    db = this.result;
+    console.log("openDb DONE");
+};
+
+request.onupgradeneeded = function (event) {
+    db = event.target.result;
+
+
+    var objectStore = db.createObjectStore("translados", { autoIncrement: true });
+
+    // Se crea un índice para buscar clientes por nombre. Se podrían tener duplicados
+    // por lo que no se puede usar un índice único.
+    objectStore.createIndex("producto", "producto", { unique: false });
+    objectStore.createIndex("cantidad", "cantidad", { unique: false });
+    objectStore.createIndex("destinatario", "destinatario", { unique: false });
+    objectStore.createIndex("fecha", "fecha", { unique: false });
+    objectStore.createIndex("motivo", "motivo", { unique: false });
+
+    // Se usa transaction.oncomplete para asegurarse que la creación del almacén 
+    // haya finalizado antes de añadir los datos en el.
+    objectStore.transaction.oncomplete = function (event) {
+        // Guarda los datos en el almacén recién creado.
+        var transladosObjectStore = db.transaction("translados", "readwrite").objectStore("translados");
+        for (var i in transladosData) {
+            transladosObjectStore.add(transladosData[i]);
+        }
+    }
+
+};
+
+function getRadioButton() {
+    var ele = document.getElementsByName('optradio');
+    console.log(ele);
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked)
+            return ele[i].value;
+    }
+};
+
+function addTranslado() {
+    if (!window.navigator.onLine) {
+        console.log("Agregar un translado");
+        var producto = document.getElementById("producto").value;
+        var cantidad = document.getElementById("cantidad").value;
+        var destinatario = getRadioButton();
+
+        var fecha = document.getElementById("fecha").value;
+        var motivo = document.getElementById("motivo").value;
+
+        var obj = {
+            producto: producto, cantidad: cantidad, destinatario: destinatario, fecha: fecha, motivo: motivo
+        };
+        console.log(obj);
+
+        var transaction = db.transaction(["translados"], "readwrite");
+        var objectStore = transaction.objectStore("translados");
+
+        var req;
+        try {
+            req = objectStore.add(obj);
+        } catch (e) {
+            console.log("error " + e.errorCode);
+            throw e;
+        }
+
+        req.onsuccess = function (evt) {
+            console.log("Se ha agregado el translado a la base de datos :)");
+        }
+
+        req.onerror = function () {
+            console.error("error al agregar el translado :(", this.error);
+        }
+    }
+};
+
+function connectionAlert() {
+
+    var transaction = db.transaction(["translados"], "readwrite");
+    var objectStore = transaction.objectStore("translados");
+    var trans = "Se enviarán los siguientes datos recuperados mientras no había conexión:\n";
+    objectStore.openCursor().onsuccess = function (event) {
+        var cursor = event.target.result;
+        if (cursor) {
+            console.log(cursor.value.producto);
+            trans += "producto: " + cursor.value.producto + ", cantidad: " + cursor.value.cantidad + "\n";
+
+            cursor.continue();
+        }
+        else {
+            console.log("No more entries!");
+            alert(trans);
+            objectStore.clear();
+        }
+    }
+
+
+};
+
+
+
